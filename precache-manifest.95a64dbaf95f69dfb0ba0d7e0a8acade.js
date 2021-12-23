ba8139933ec513cf6ab9f794688acfcf609a999c
Oleksandr <s471652@gmail.com>
2021-12-23 22:25:45 +0200

    Updates

---
+++ precache-manifest.95a64dbaf95f69dfb0ba0d7e0a8acade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b43654f119c5c184dad057caa911f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca5a95cacb4fd25ed6da",
+    "url": "/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "eeadae3df33f09853cb7",
+    "url": "/static/js/2.70693b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.70693b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5a95cacb4fd25ed6da",
+    "url": "/static/js/main.f4aa9b8d.chunk.js"
+  },
+  {
+    "revision": "8c0ed676e7fc0e04cf11",
+    "url": "/static/js/runtime-main.81dc9e71.js"
+  }
+]);
